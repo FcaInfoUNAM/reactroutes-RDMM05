080c5d25dd809aaf1df4ffbe0969b990c661a64a
RDMM05 <robdmen@gmail.com>
2025-05-23 22:15:36 -0600

    propiedades de los componentes

---
+++ tailwind-app/src/NotFound.jsx
@@ -0,0 +1,12 @@
+function NotFound() {
+    return (
+      <div className="text-center mt-20">
+        <h1 className="text-6xl font-bold text-red-600">404</h1>
+        <p className="text-xl mt-4">Página no encontrada</p>
+        <p className="text-gray-500">La URL que has ingresado no existe.</p>
+      </div>
+    );
+  }
+
+  export default NotFound;
+  
